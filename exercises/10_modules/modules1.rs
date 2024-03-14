9f6226037df357efc828f485b0b73f8136e80c13
Vedant <28388096+Vedu1996@users.noreply.github.com>
2024-03-14 18:34:26 +0530

    Strings and Modules

--- exercises/10_modules/modules1.rs
+++ exercises/10_modules/modules1.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
@@ -11,6 +10,7 @@ mod sausage_factory {
         String::from("Ginger")
     }
 
+    pub(super)
     fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
